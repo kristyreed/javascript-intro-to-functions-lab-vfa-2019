70c4fe2f65fe0d777fb9094bf44a557cb963cfd9
Kristy Reed <kristy.reed22@gmail.com>
2019-06-16 09:05:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  return console.log(string.toUpperCase);}
+
+function logWhisper(string){
+  return string.toLowerCase }
+  console.log(logWhisper())
+
+function sayHiToGrandma(string){
+  var uppercase = "HELLO"
+  var lowercase = "hello"
+  if (string === "I love you, Grandma.") {
+    return "I love you, too." }
+  if (string === uppercase){
+    return "YES INDEED!" }
+  if (string === lowercase){
+    return "I can\'t hear you!" }
+}
